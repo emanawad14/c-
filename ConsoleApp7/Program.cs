a90944b25bb3077cf01a6f7d1e94531a7c09df9b
emanawad14 <emanelnady2001@gmail.com>
2024-12-12 01:26:49 +0200

    first commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿// 1 - write a c sharp program to print the sum of two numbers .
+// string interpolation.
+
+/*
+int number1 = 30;
+int number2  = 40;
+int sum = number1 + number2;
+Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+
+*/
+
+//  2 - write a c sharp program to print the comparing
+// result between three interger values at least 5 complex expression .
+/*
+int a = 10;
+int b = 20;
+int c = 15;
+Console.WriteLine($"Are All The Numbers Equal ?  {a == b && b==c}");
+Console.WriteLine($"Is 'a' is Largest ?  {a>b && b>c}");
+Console.WriteLine($"Is 'c' is  Smallest ?  {c<b && c<a}");
+Console.WriteLine($"Is 'c' between 'a' and 'b' ?  { (c>a && c<b) ||(c<a && c>b) }");
+Console.WriteLine($"Are at  Least Two Number Equal ? {a == b ||b ==c || c==a} ");
+
+ */
+
+// write a c sharp program to print the result the specified operation .Test Data 
+
+Console.WriteLine(-1+4*6);
+Console.WriteLine((35 + 5) % 7);
+Console.WriteLine(14 + -4 * 6 / 11);
+Console.WriteLine(2+15 /6*1-7%2);
